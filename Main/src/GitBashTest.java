c71cedf01e86c18e06843a4cf755c8e2eac91dce
Lamskov <dima.lamskoff@yandex.ru>
2025-03-24 08:54:01 +0300

    Добавлена структура проекта с HelloWorld и HelloGit

--- Main/src/GitBashTest.java
+++ Main/src/GitBashTest.java
@@ -1,6 +1,6 @@
 public class GitBashTest {
     public static void main(String[] args) {
-        System.out.println("GitBashTest_1");
-        System.out.println("GitBashTest_2");
+        System.out.println("Привет, Git и GitHub!");
+        System.out.println("Я начинаю изучение Java!");
     }
 }
